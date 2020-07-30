ccb964637a37b985830daeec85de5c3c6c7b3f91
SMAshhar <63975782+SMAshhar@users.noreply.github.com>
2020-07-31 00:22:29 +0800

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,7 @@
+import datetime
+currentDate = datetime.date.today()
+print(currentDate)
+print(currentDate.year)
+print(currentDate.month)
+print(currentDate.day)
+print(currentDate.strftime('%d %b,%Y'))
