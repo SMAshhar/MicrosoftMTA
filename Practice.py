b98c0c2e565bbf8654b4ee0dbbeb233d65826546
SMAshhar <63975782+SMAshhar@users.noreply.github.com>
2020-07-31 12:08:11 +0500

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -1,7 +1,18 @@
+# import datetime
+# currentDate = datetime.date.today()
+# print(currentDate)
+# print(currentDate.year)
+# print(currentDate.month)
+# print(currentDate.day)
+# print(currentDate.strftime('I have been working since %a, %dth of %b,%y'))
+# asd = datetime.date(2050, 8, 29)
+# print(asd.strftime("Yeehaw... Join Us on %dth of %B, %Y for a lovely evening"))
+# print(asd.strftime("THe thing is %d of %B is our last chance to do anything. After that, the year %Y will no longer be valid for us"))
+# print(asd.strftime("Yes Mirha is right, she called abba to everyone.... "))
+
 import datetime
-currentDate = datetime.date.today()
-print(currentDate)
-print(currentDate.year)
-print(currentDate.month)
-print(currentDate.day)
-print(currentDate.strftime('%d %b,%Y'))
+
+brithday = input("What is your birthday? (mm/dd/yyyy)")
+birthdate = datetime.datetime.strptime(brithday, "%m/%d/%Y").date()
+print(birthdate.day, birthdate.month, birthdate.year)
+
